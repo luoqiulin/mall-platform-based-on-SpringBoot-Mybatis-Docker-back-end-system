a624ab0044e16a6d7cfb0ad31dd0b942db2dfd88
Qiulin Luo <1109925007@qq.com>
2023-11-07 04:44:38 -0800

    All
    Deployed on mac

---
+++ mall-admin/src/main/java/com/macro/mall/dao/PmsProductCategoryAttributeRelationDao.java
@@ -0,0 +1,17 @@
+package com.macro.mall.dao;
+
+import com.macro.mall.model.PmsProductCategoryAttributeRelation;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * 商品分类和属性关系自定义Dao
+ * Created by qiulin.
+ */
+public interface PmsProductCategoryAttributeRelationDao {
+    /**
+     * 批量创建
+     */
+    int insertList(@Param("list") List<PmsProductCategoryAttributeRelation> productCategoryAttributeRelationList);
+}
